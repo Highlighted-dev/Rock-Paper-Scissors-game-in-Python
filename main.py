e3e30b459e6d2ac99d50594c57952b8cbab98b74
n3wbi-dev <bartosz.korz@gmail.com>
2020-10-30 22:46:39 +0100

    Added main code. Game should work now.

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+i = 0
+
+while True:
+    userInput = input("Rock, Paper or Scissors: ")
+    userInput = userInput.capitalize()
+    if userInput.lower() == "exit":
+        break
+
+    randomIntForBot = random.randint(0,2)
+    rockPaperScissorsArray = ["Rock","Paper","Scissors"]
+    botChoice = rockPaperScissorsArray[randomIntForBot]
+
+    print(f"You: {userInput}!")
+    print(f"Bot: {botChoice}!")
+    if not userInput in rockPaperScissorsArray:
+        print("You have to type rock, paper or scissors! \n")
+    elif userInput == botChoice:
+        print("Result: Draw! \n")
+    elif userInput == "Rock" and botChoice == "Scissors" or userInput == "Scissors" and botChoice == "Rock" or userInput == "Paper" and botChoice == "Rock":
+        print("Result: You won! \n")
+    else:
+        print("Result: Bot  won! \n")
+
+
+    i = 0
+
+
+
+
+
+
